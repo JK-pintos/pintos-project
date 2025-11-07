c4b988609c7c73c09da5354387ff281713095d32
Jeongns <jhlee030313@gmail.com>
2025-11-07 21:56:28 +0900

    코드 정리

--- pintos/devices/timer.c
+++ pintos/devices/timer.c
@@ -31,7 +31,7 @@ static intr_handler_func timer_interrupt;
 static bool too_many_loops(unsigned loops);
 static void busy_wait(int64_t loops);
 static void real_time_sleep(int64_t num, int32_t denom);
-static void wake_sleeping_threads();
+static void wake_sleeping_threads(void);
 static bool sleep_list_order(struct list_elem* e1, struct list_elem* e2, void* aux);
 
 /* Sets up the 8254 Programmable Interval Timer (PIT) to
@@ -90,12 +90,14 @@ int64_t timer_elapsed(int64_t then) { return timer_ticks() - then; }
 /* Suspends execution for approximately TICKS timer ticks. */
 void timer_sleep(int64_t ticks) {
     if (ticks <= 0) return;
+    ASSERT(intr_get_level() == INTR_ON);
     enum intr_level old_level = intr_disable();
 
     struct thread* cur_thread = thread_current();
     cur_thread->wakeup_tick = timer_ticks() + ticks;
     list_insert_ordered(&sleep_list, &cur_thread->elem, sleep_list_order, NULL);
     thread_block();
+
     intr_set_level(old_level);
 }
 
@@ -114,9 +116,8 @@ void timer_print_stats(void) { printf("Timer: %" PRId64 " ticks\n", timer_ticks(
 /* Timer interrupt handler. */
 static void timer_interrupt(struct intr_frame* args UNUSED) {
     ticks++;
+    wake_sleeping_threads();
     thread_tick();
-
-    wake_sleeping_threads(ticks);
 }
 
 /* Returns true if LOOPS iterations waits for more than one timer
@@ -171,7 +172,10 @@ static void real_time_sleep(int64_t num, int32_t denom) {
     }
 }
 
-static void wake_sleeping_threads() {
+/// @brief  
+/// 현재 시각(ticks)에 도달한 스레드들을 깨워 READY 상태로 전환한다.  
+/// (sleep_list의 맨 앞부터 검사하며, wakeup_tick이 아직 안 된 스레드는 남겨둔다.)
+static void wake_sleeping_threads(void) {
     enum intr_level old_level = intr_disable();
     while (!list_empty(&sleep_list)) {
         struct thread* cur_thread = list_entry(list_front(&sleep_list), struct thread, elem);
@@ -182,7 +186,17 @@ static void wake_sleeping_threads() {
     intr_set_level(old_level);
 }
 
+/// @brief  
+/// 두 스레드의 wakeup_tick 값을 비교하여 정렬 순서를 결정한다.  
+/// (timer_sleep에서 list_insert_ordered()에 사용됨)
+///
+/// @param e1 첫 번째 리스트 요소의 포인터
+/// @param e2 두 번째 리스트 요소의 포인터
+/// @param aux 추가 인자(사용하지 않음)
+/// @return  
+/// e1의 wakeup_tick이 e2보다 작으면 true, 아니면 false
 static bool sleep_list_order(struct list_elem* e1, struct list_elem* e2, void* aux) {
-    return list_entry(e1, struct thread, elem)->wakeup_tick <
-           list_entry(e2, struct thread, elem)->wakeup_tick;
+    struct thread* thread1 = list_entry(e1, struct thread, elem);
+    struct thread* thread2 = list_entry(e2, struct thread, elem);
+    return thread1->wakeup_tick < thread2->wakeup_tick;
 }
