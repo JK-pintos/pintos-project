28c8fef347ab95d4b973ab4066572c286f7e7d8a
SangChunn <sang9568@gmail.com>
2025-11-08 10:27:25 +0900

    Priority test code pass

--- pintos/threads/synch.c
+++ pintos/threads/synch.c
@@ -66,7 +66,7 @@ sema_down (struct semaphore *sema) {
 
 	old_level = intr_disable ();
 	while (sema->value == 0) {
-		list_push_back (&sema->waiters, &thread_current ()->elem);
+		list_insert_ordered(&sema->waiters, &thread_current()->elem, thread_priority_order, NULL);
 		thread_block ();
 	}
 	sema->value--;
@@ -103,18 +103,16 @@ sema_try_down (struct semaphore *sema) {
 
    This function may be called from an interrupt handler. */
 void
-sema_up (struct semaphore *sema) {
-	enum intr_level old_level;
-
-	ASSERT (sema != NULL);
-
-	old_level = intr_disable ();
-	if (!list_empty (&sema->waiters))
-		thread_unblock (list_entry (list_pop_front (&sema->waiters),
-					struct thread, elem));
+sema_up(struct semaphore *sema) {
+	enum intr_level old_level = intr_disable();
+	ASSERT(sema != NULL);
 	sema->value++;
-	intr_set_level (old_level);
-}
+    if (!list_empty(&sema->waiters)) {
+        list_sort(&sema->waiters, thread_priority_order, NULL);
+        thread_unblock(list_entry(list_pop_front(&sema->waiters), struct thread, elem));
+    }
+    intr_set_level(old_level);
+   }
 
 static void sema_test_helper (void *sema_);
 
--- pintos/threads/thread.c
+++ pintos/threads/thread.c
@@ -65,6 +65,8 @@ static void do_schedule(int status);
 static void schedule(void);
 static tid_t allocate_tid(void);
 
+bool thread_priority_order(const struct list_elem* a, const struct list_elem* b, void* aux);
+
 /* Returns true if T appears to point to a valid thread. */
 #define is_thread(t) ((t) != NULL && (t)->magic == THREAD_MAGIC)
 
@@ -227,8 +229,14 @@ void thread_unblock(struct thread* t) {
 
     old_level = intr_disable();
     ASSERT(t->status == THREAD_BLOCKED);
-    list_push_back(&ready_list, &t->elem);
+
+    list_insert_ordered(&ready_list, &t->elem, thread_priority_order, NULL);
     t->status = THREAD_READY;
+
+    if (thread_current() != idle_thread && t->priority > thread_current()->priority) {
+        thread_yield();
+    }
+
     intr_set_level(old_level);
 }
 
@@ -280,13 +288,23 @@ void thread_yield(void) {
     ASSERT(!intr_context());
 
     old_level = intr_disable();
-    if (curr != idle_thread) list_push_back(&ready_list, &curr->elem);
+    if (curr != idle_thread) list_insert_ordered(&ready_list, &curr->elem, thread_priority_order, NULL);
     do_schedule(THREAD_READY);
     intr_set_level(old_level);
 }
 
 /* Sets the current thread's priority to NEW_PRIORITY. */
-void thread_set_priority(int new_priority) { thread_current()->priority = new_priority; }
+void thread_set_priority(int new_priority) { 
+    enum intr_level old_level = intr_disable();
+    thread_current()->priority = new_priority; 
+
+    if (!list_empty(&ready_list)){
+        struct thread *next = list_entry(list_front(&ready_list), struct thread, elem);
+        if (next->priority > thread_current()->priority) 
+            thread_yield();
+    }
+    intr_set_level(old_level);
+}
 
 /* Returns the current thread's priority. */
 int thread_get_priority(void) { return thread_current()->priority; }
@@ -545,3 +563,11 @@ static tid_t allocate_tid(void) {
 
     return tid;
 }
+
+/* Compare two threads by priority */
+bool thread_priority_order(const struct list_elem* a, const struct list_elem* b, void* aux){
+    const struct thread* thread1 = list_entry(a, struct thread, elem);
+    const struct thread* thread2 = list_entry(b, struct thread, elem);
+    return thread1->priority > thread2->priority;
+}
+ 
