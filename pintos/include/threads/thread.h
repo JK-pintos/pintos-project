638e3815636f95c6743f8c2d8b81206f386fa1f6
SangChunn <sang9568@gmail.com>
2025-11-12 21:05:58 +0900

    mlfqs finishgit add .!

--- pintos/include/threads/thread.h
+++ pintos/include/threads/thread.h
@@ -100,7 +100,7 @@ struct thread {
 	
 	/* Shared between thread.c and synch.c. */
 	struct list_elem elem;              /* List element. */
-
+	struct list_elem allelem;
 	int64_t wakeup_tick;
 
 	int nice;
@@ -155,6 +155,6 @@ int thread_get_load_avg (void);
 
 void do_iret (struct intr_frame *tf);
 
-bool thread_priority_max(struct list_elem* e1, struct list_elem* e2, void* aux);
+bool thread_priority_max(const struct list_elem* e1, const struct list_elem* e2, void* aux);
 
 #endif /* threads/thread.h */
