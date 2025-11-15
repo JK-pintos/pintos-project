adcc448f7e9df5a009675e9d9429563e716b9301
Jeongns <jhlee030313@gmail.com>
2025-11-15 19:31:24 +0900

    잘못된 header 삭제

--- pintos/include/threads/thread.h
+++ pintos/include/threads/thread.h
@@ -145,10 +145,6 @@ void thread_print_stats(void);
 typedef void thread_func(void* aux);
 tid_t thread_create(const char* name, int priority, thread_func*, void*);
 
-void asdf(struct child_info* child_info, struct thread* t);
-
-void ㅁㄴㅇㄹ(struct child_info* child_info);
-
 void thread_block(void);
 void thread_unblock(struct thread*);
 
