276477f2cf2b00995579a90f6594c70f18d61ff7
Jeongns <jhlee030313@gmail.com>
2025-11-18 16:21:17 +0900

    안쓰는 함수 삭제

--- pintos/userprog/validate.h
+++ pintos/userprog/validate.h
@@ -2,5 +2,3 @@
 #include <stddef.h>
 
 bool validate_ptr(const void* uaddr, bool write);
-bool validate_buffer(const void* uaddr, size_t size, bool write);
-bool validate_string(const char* uaddr, bool write);
