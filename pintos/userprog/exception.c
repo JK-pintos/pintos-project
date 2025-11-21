9a462957478a0a3cedb3f6515503c1207c9a5667
SangChunn <sang9568@gmail.com>
2025-11-21 22:34:18 +0900

    fix/page_fault

--- pintos/userprog/exception.c
+++ pintos/userprog/exception.c
@@ -5,7 +5,6 @@
 #include "threads/interrupt.h"
 #include "threads/thread.h"
 #include "intrinsic.h"
-#include "userprog/syscall.h"
 
 /* Number of page faults processed. */
 static long long page_fault_cnt;
@@ -140,7 +139,8 @@ page_fault (struct intr_frame *f) {
 	not_present = (f->error_code & PF_P) == 0;
 	write = (f->error_code & PF_W) != 0;
 	user = (f->error_code & PF_U) != 0;
-	syscall_exit(-1);
+	thread_current()->my_entry->exit_status = -1;
+	thread_exit();
 #ifdef VM
 	/* For project 3 and later. */
 	if (vm_try_handle_fault (f, fault_addr, user, write, not_present))
