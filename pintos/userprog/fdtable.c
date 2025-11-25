81cea78ef2267d5d0c8edb21551a7f49c61d75c7
yerpark <dstforye@gmail.com>
2025-11-25 20:50:14 +0900

    fail..

--- pintos/userprog/fdtable.c
+++ pintos/userprog/fdtable.c
@@ -11,17 +11,18 @@ void init_std_fds() {
     if (!stdout_entry) PANIC("malloc failed\n");
 }
 
-void fdt_list_init(struct thread* t) {
+bool fdt_list_init(struct thread* t) {
     struct fdt_block* first_fdt_block;
 
     list_init(&(t->fdt_block_list));
     first_fdt_block = (struct fdt_block*)calloc(1, sizeof(struct fdt_block));
-    if (!first_fdt_block) PANIC("malloc failed\n");
+    if (!first_fdt_block) return false;
 
     first_fdt_block->entry[0] = stdin_entry;
     first_fdt_block->entry[1] = stdout_entry;
     first_fdt_block->available_idx = 2;
     list_push_back(&(t->fdt_block_list), &(first_fdt_block->elem));
+    return true;
 }
 
 int fd_allocate(struct thread* t, struct file* f) {
@@ -47,7 +48,11 @@ int fd_allocate(struct thread* t, struct file* f) {
         }
     }
 
-    fdt_block_append(t);
+    if (false == fdt_block_append(t))
+    {
+        file_close(f);
+        return -1;
+    }
     block = list_entry(list_prev(tail), struct fdt_block, elem);
     block->entry[0] = f;
     block->available_idx = 1;
@@ -71,7 +76,7 @@ struct fdt_block* get_fd_block(struct thread* t, int* fd) {
         block_start_fd += FD_BLOCK_MAX;
     }
 
-    if (block_start_fd + FD_BLOCK_MAX <= *fd) return NULL;
+    if (e == tail || block_start_fd + FD_BLOCK_MAX <= *fd) return NULL;
 
     block = list_entry(e, struct fdt_block, elem);
     *fd = *fd - block_start_fd;  // 블록 안의 fd로 재조정
@@ -93,7 +98,7 @@ void fd_close(struct thread* t, int fd) {
     struct fdt_block* block;
     struct file* close_entry;
 
-    block = get_fd_block(thread_current(), &fd);
+    block = get_fd_block(t, &fd);
     if (!block) return;
 
     close_entry = block->entry[fd];
@@ -114,18 +119,20 @@ void fdt_list_cleanup(struct thread* t) {
         block = list_entry(e, struct fdt_block, elem);
         for (i = 0; i < FD_BLOCK_MAX; i++) {
             entry = block->entry[i];
-            if (entry) file_close(entry);
+            if (entry && entry != stdin_entry && entry != stdout_entry) file_close(entry);
+            block->entry[i] = NULL;
         }
-        free(block);
+        free (block);
     }
 }
 
-void fdt_block_append(struct thread* t) {
+bool fdt_block_append(struct thread* t) {
     struct fdt_block* block;
 
     block = (struct fdt_block*)calloc(1, sizeof(struct fdt_block));
-    if (!block) PANIC("malloc failed\n");
+    if (!block) return false;
     list_push_back(&(t->fdt_block_list), &(block->elem));
+    return true;
 }
 
 void scan_for_next_fd(struct fdt_block* block) {
@@ -145,4 +152,71 @@ void scan_for_next_fd(struct fdt_block* block) {
         idx++;
     }
     block->available_idx = -1;
+}
+
+bool    duplicate_fdt_block(struct fdt_block *parent_block, struct fdt_block *child_block, struct thread *child)
+{
+    struct file     *parent_entry;
+    struct file     *new_entry;
+    int             i = 0;
+
+    if (!child_block)
+    {
+        child_block = (struct fdt_block *)calloc(1, sizeof(struct fdt_block));
+        if (!child_block)
+            return false;
+        list_push_back(&(child->fdt_block_list), &(child_block->elem));
+    }
+
+    while (i < FD_BLOCK_MAX)
+    {
+        if (parent_block->entry[i] == stdin_entry || \
+            parent_block->entry[i] == stdout_entry)
+            child_block->entry[i] = parent_block->entry[i];
+        else if (parent_block->entry[i])
+        {
+            new_entry = file_duplicate(parent_block->entry[i]);
+            if (!new_entry)
+                return false;
+            child_block->entry[i] = new_entry;
+        }
+        i++;
+    }
+    child_block->available_idx = parent_block->available_idx;
+    return true;
+}
+
+bool    duplicate_fdt_block_list(struct thread *parent, struct thread *child)
+{
+    struct list_elem    *parent_e;
+    struct list_elem    *child_e;
+    struct list_elem    *parent_tail;
+    struct list_elem    *child_tail;
+    struct fdt_block    *parent_block;
+    struct fdt_block    *child_block = NULL;
+
+    parent_e = list_begin(&(parent->fdt_block_list));
+    parent_tail = list_tail(&(parent->fdt_block_list));
+    child_e = list_begin(&(child->fdt_block_list));
+    child_tail = list_tail(&(child->fdt_block_list));
+
+    while (parent_e != parent_tail)
+    {
+        parent_block = list_entry(parent_e, struct fdt_block, elem);
+        if (child_e != child_tail)
+            child_block = list_entry(child_e, struct fdt_block, elem);
+
+        if (false == duplicate_fdt_block(parent_block, child_block, child))
+        {
+            fdt_list_cleanup(child);
+            return false;
+        }
+
+        if (child_e != child_tail)
+            child_e = list_next(child_e);
+        else
+            child_block = NULL;
+        parent_e = list_next(parent_e);
+    }
+    return true;
 }
